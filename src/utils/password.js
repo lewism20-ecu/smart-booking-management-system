24f3cbde8e29437fd66dff8a1239d2da6df50286
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-03-23 20:42:10 -0400

    implement password hashing utility

---
+++ src/utils/password.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 19;
+
+async function hashPassword(plain) {
+    return bcrypt.hash(plain, SALT_ROUNDS);
+}
+
+async function comparePassword(plain, hash) {
+    return bcrypt.compare(plain, hash);
+}
+
+module.exports = {
+    hashPassword,
+    comparePassword,
+};
