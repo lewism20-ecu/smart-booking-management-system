2a5637c177369bcb56c218f6a1f1a185de7a5771
Curtis Moore <moorecu17@students.ecu.edu>
2026-03-31 16:33:12 -0400

    refactor: use utils/password and utils/validation in authController instead of direct bcrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,9 @@
-const bcrypt    = require('bcrypt');
-const { signToken } = require('../utils/jwt');
-const userModel = require('../models/userModel');
+const { signToken }                    = require('../utils/jwt');
+const { hashPassword, comparePassword } = require('../utils/password');
+const { isStrongPassword }             = require('../utils/validation');
+const userModel                        = require('../models/userModel');
 
-/**
- * POST /auth/signup
- * Creates a new user account
- */
-exports.signup = async (req, res, next) => {
+async function signup(req, res, next) {
   try {
     const { email, password } = req.body;
 
@@ -25,14 +22,14 @@ exports.signup = async (req, res, next) => {
       });
     }
 
-    if (password.length < 8) {
+    if (!isStrongPassword(password)) {
       return res.status(400).json({
         error: 'BadRequest',
         message: 'Password must be at least 8 characters.'
       });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await hashPassword(password);
     const user = await userModel.createUser(email, hashedPassword);
 
     res.status(201).json(user);
@@ -45,13 +42,9 @@ exports.signup = async (req, res, next) => {
     }
     next(err);
   }
-};
+}
 
-/**
- * POST /auth/login
- * Authenticates a user and returns a JWT
- */
-exports.login = async (req, res, next) => {
+async function login(req, res, next) {
   try {
     const { email, password } = req.body;
 
@@ -64,7 +57,7 @@ exports.login = async (req, res, next) => {
 
     const user = await userModel.findUserByEmail(email);
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!user || !(await comparePassword(password, user.password))) {
       return res.status(401).json({
         error: 'Unauthorized',
         message: 'Invalid credentials.'
@@ -87,4 +80,6 @@ exports.login = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+}
+
+module.exports = { signup, login };
